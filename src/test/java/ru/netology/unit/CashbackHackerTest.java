44deea32420061f9d9ac2f98b624da21bff3ec99
Kirillova Olesya <olesyak-85@mail.ru>
2021-10-15 20:01:43 +0300

    added test junit4-platform

--- src/test/java/ru/netology/unit/CashbackHackerTest.java
+++ src/test/java/ru/netology/unit/CashbackHackerTest.java
@@ -1,5 +1,7 @@
 package ru.netology.unit;
 
+import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
 public class CashbackHackerTest{
@@ -15,6 +17,16 @@ public class CashbackHackerTest{
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void shouldGetAmountUnder() {
+        int amount = 0;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+
     @org.junit.Test
     public void shouldGetAmountOver1000() {
         int amount = 1850;
@@ -25,8 +37,30 @@ public class CashbackHackerTest{
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void shouldGetAmountOver() {
+        int amount = 1005;
+
+        int actual = service.remain(amount);
+        int expected = 995;
+
+        assertEquals(actual, expected);
+    }
+
     @org.junit.Test
     public void shouldGetAmountEqually1000(){
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+
+        //при проверке на граничные значения не проходит. неверно описана логика
+    }
+
+    @Test
+    public void shouldGetAmountEqually(){
         int amount = 1000;
 
         int actual = service.remain(amount);
